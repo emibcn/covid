7575da6f0eba0cc92ffef086fcf8656d8acde7e9
emibcn <emi@algorismia.com>
2020-09-24 02:48:46 +0200

    Available Languages: Redirect all languages to catalan

--- app/src/i18n/available.js
+++ app/src/i18n/available.js
@@ -1,19 +1,30 @@
 /*
+import ca_es from "./ca-es.lang.js";
 import es_es from "./es-es.lang.js";
 import en_en from "./en-en.lang.js";
-*/
-import ca_es from "./ca-es.lang.js";
 
 // Exports each language with more locale codes
 export default {
-/*
   'en'   : en_en,
   'en-en': en_en,
   'en-us': en_en,
   'en-au': en_en,
-*/
   'ca'   : ca_es,
   'ca-es': ca_es,
-//  'es'   : es_es,
-//  'es-es': es_es,
+  'es'   : es_es,
+  'es-es': es_es,
+};
+*/
+import ca_es from "./ca-es.lang.js";
+
+// Only catalan available by the moment
+export default {
+  'en'   : ca_es,
+  'en-en': ca_es,
+  'en-us': ca_es,
+  'en-au': ca_es,
+  'ca'   : ca_es,
+  'ca-es': ca_es,
+  'es'   : ca_es,
+  'es-es': ca_es,
 };
