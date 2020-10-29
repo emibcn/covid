eed805495d9adf71f4f18a76a240bc3dd136da67
emibcn <emi@algorismia.com>
2020-10-29 19:50:00 +0100

    Tests: testSetup: Remove export, add comment with related issue and fix indentation

--- app/src/testSetup.js
+++ app/src/testSetup.js
@@ -1,10 +1,10 @@
+// https://github.com/enzymejs/enzyme/issues/1626#issuecomment-398588616
 global.document.createRange = () => ({
   setStart: () => {},
   setEnd: () => {},
   commonAncestorContainer: {
-      nodeName: 'BODY',
-      ownerDocument: document
+    nodeName: 'BODY',
+    ownerDocument: document
   },
   createContextualFragment: jest.fn
 });
-export default {};
