1c143f5fc50ae75579783a14c2d520b6d838bef5
emibcn <emi@algorismia.com>
2020-10-30 17:41:49 +0100

    Tests: Mock media queries to test some components

--- app/src/testSetup.js
+++ app/src/testSetup.js
@@ -1,3 +1,5 @@
+import matchMediaPolyfill from 'mq-polyfill'
+
 // https://github.com/enzymejs/enzyme/issues/1626#issuecomment-398588616
 global.document.createRange = () => ({
   setStart: () => {},
@@ -8,3 +10,16 @@ global.document.createRange = () => ({
   },
   createContextualFragment: jest.fn
 });
+
+// Allow testing MediaQuery: https://github.com/testing-library/react-testing-library/issues/353#issuecomment-510074776
+beforeAll(() => {
+  matchMediaPolyfill(window)
+  window.resizeTo = function resizeTo(width, height) {
+    Object.assign(this, {
+      innerWidth: width,
+      innerHeight: height,
+      outerWidth: width,
+      outerHeight: height,
+    }).dispatchEvent(new this.Event('resize'))
+  }
+})
