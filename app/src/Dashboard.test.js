41ed1cce80fb0859c822981e45ecc8adf24ffa8a
emibcn <emi@algorismia.com>
2020-10-30 17:43:06 +0100

    Tests: Dashboard: Add AppBar testing. Add props.newServiceWorkerDetected + mediaQuery testing

--- app/src/Dashboard.test.js
+++ app/src/Dashboard.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, createEvent, fireEvent, act, screen } from '@testing-library/react';
+import { render, createEvent, fireEvent, act, screen, cleanup } from '@testing-library/react';
 import './testSetup';
 import TestRenderer from 'react-test-renderer';
 
@@ -7,14 +7,56 @@ import { MemoryRouter as Router } from 'react-router-dom';
 import Dashboard, {Copyright} from './Dashboard';
 import Menu from './Menu';
 
+// Mock @material-ui/core/AppBar to add `data-testid` attrib
+jest.mock("@material-ui/core/AppBar", () => {
+  return {
+    __esModule: true,
+    default: ({children, className}) => <div className={className} data-testid="app-bar" >{ children }</div>,
+  };
+});
+
 test('renders copyright link', () => {
   const { getByText } = render(<Router><Dashboard onLoadNewServiceWorkerAccept={() => {}} /></Router>);
   const copyrightElement = getByText(/Source code of/i);
   expect(copyrightElement).toBeInTheDocument();
 });
 
-test('opens about dialog on initial hash', () => {
+test('renders closed AppBar', () => {
   const dashboard = render(<Router><Dashboard onLoadNewServiceWorkerAccept={() => {}} /></Router>);
+  const appBar = dashboard.getByTestId('app-bar');
+
+  // Test computed style to NOT have marginLeft
+  const styles = window.getComputedStyle(appBar);
+  expect(styles.marginLeft).not.toBe('240px');
+
+  // Test assigned class with clsx
+  const foundKlass = [...appBar.classList].find( klass => /^makeStyles-appBar-/.test(klass));
+  expect(foundKlass).toBeTruthy();
+  expect(appBar.classList.contains(foundKlass)).toBe(true);
+});
+
+test('alerts user when new service worker is detected', () => {
+
+  act( () => {
+    const dashboard = render(<Router><Dashboard newServiceWorkerDetected={true} onLoadNewServiceWorkerAccept={() => {}} /></Router>);
+    const updateElement = dashboard.getByText('1'); // Detect menu badge
+    expect(updateElement.classList.contains('MuiBadge-invisible')).toBe(true);
+    dashboard.unmount();
+  });
+
+
+  act( (done) => {
+    window.resizeTo(400, 200);
+    // Wait for resize to take full effect (more than one event loop?)
+    setTimeout( () => {
+      const dashboard = render(<Router><Dashboard newServiceWorkerDetected={true} onLoadNewServiceWorkerAccept={() => {}} /></Router>);
+      const updateElement = dashboard.getByText('1'); // Detect menu badge
+      expect(updateElement.classList.contains('MuiBadge-invisible')).toBe(false);
+      dashboard.unmount();
+      done();
+    }, 1000);
+  });
+
 });
 
 test('opens about dialog on initial location hash', () => {
