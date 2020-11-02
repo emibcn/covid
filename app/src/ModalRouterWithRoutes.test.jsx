8f0ad15353b58e167afc51e2802e9ea72fb1c1ba
emibcn <emi@algorismia.com>
2020-11-02 15:41:32 +0100

    Tests: ModalRouterWithRoutes: Add test for close button not shown

--- app/src/ModalRouterWithRoutes.test.jsx
+++ app/src/ModalRouterWithRoutes.test.jsx
@@ -18,29 +18,41 @@ jest.mock("./ErrorCatcher", () => {
 
 test('opens none section', async () => {
   const modal = render(
-    <Router initialEntries={[ '#' ]}><ModalRouterWithRoutes routeProps={{ language: "en-us" }} /></Router>);
-  // TODO: Needs to fail
-  //  - Need to find the element and be ok not finding it (and it does not)
-  //  - If found, test should fail (and it does not)
-  //const closeButton = screen.getByText(/ModalRouter.Close/);
-  //expect(closeButton).toBeInTheDocument();
+    <Router initialEntries={[ '#' ]}>
+      <ModalRouterWithRoutes routeProps={{ language: "en-us" }} />
+    </Router>
+  );
+
+  // Close button should NOT be in screen
+  expect(
+    () => screen.getByText(/ModalRouter.Close/)
+  ).toThrowError(/Unable to find an element/);
 });
 
 test('opens about section', async () => {
   const modal = render(
-    <Router initialEntries={[ '#about' ]}><ModalRouterWithRoutes routeProps={{ language: "en-us" }} /></Router>);
+    <Router initialEntries={[ '#about' ]}>
+      <ModalRouterWithRoutes routeProps={{ language: "en-us" }} />
+    </Router>
+  );
+
   const closeButton = screen.getByText(/ModalRouter.Close/);
   expect(closeButton).toBeInTheDocument();
+
   const page = screen.getByText(/AboutPage/);
   expect(page).toBeInTheDocument();
 });
 
 test('opens language selector', async () => {
   const modal = render(
-    <Router initialEntries={[ '#language' ]}><ModalRouterWithRoutes routeProps={{ language: "en-us" }} /></Router>);
+    <Router initialEntries={[ '#language' ]}>
+      <ModalRouterWithRoutes routeProps={{ language: "en-us" }} />
+    </Router>
+  );
+
   const closeButton = screen.getByText(/ModalRouter.Close/);
   expect(closeButton).toBeInTheDocument();
+
   const page = screen.getByText(/LanguagePage/);
   expect(page).toBeInTheDocument();
 });
-
