64dfa53c2394d61e4a84955ced18c4fe91e5346d
emibcn <emi@algorismia.com>
2020-11-06 13:39:23 +0100

    Tests: App: split integration tests and add unit tests with a mocked Dashboard

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -2,9 +2,56 @@ import React from 'react';
 import { render, createEvent, fireEvent, act, screen } from '@testing-library/react';
 
 import './testSetup';
+import { delay } from './testHelpers';
 import App, { fixLocationHash, getDefaultLanguage } from './App';
 
-// Mock Widget/List, as this is not needed to test it here
+// Mock Dashboard and Widget/List, as this is not needed to test it here
+jest.mock("./Dashboard", () => {
+  return {
+    __esModule: true,
+    default: (props) => (
+      <div data-testid="dashboard-mock">
+        <button data-testid="dashboard-mock-fn-accept-sw" onClick={ () => props.onLoadNewServiceWorkerAccept({ detail: { registration: "tested" } }) }>
+          Accept Service Worker
+        </button>
+        <button data-testid="dashboard-mock-fn-language-change" onClick={ () => props.onLanguageChange("tested") }>
+          Change Language
+        </button>
+        <button data-testid="dashboard-mock-fn-theme-change" onClick={ () => props.onThemeChange("tested") }>
+          Change Theme
+        </button>
+        <button data-testid="dashboard-mock-fn-tutorial-seen" onClick={ () => props.onTutorialSeen("tested") }>
+          Tutorial Seen
+        </button>
+
+        <div data-testid="dashboard-mock-sw-detected">
+          { JSON.stringify(props.newServiceWorkerDetected) }
+        </div>
+        <div data-testid="dashboard-mock-language">
+          { JSON.stringify(props.language) }
+        </div>
+        <div data-testid="dashboard-mock-theme">
+          { JSON.stringify(props.theme) }
+        </div>
+        <div data-testid="dashboard-mock-tutorial-seen">
+          { JSON.stringify(props.tutorialSeen) }
+        </div>
+      </div>
+    ),
+  };
+});
+
+jest.mock("./i18n/available", () => {
+  return {
+    __esModule: true,
+    default: [
+      { key: "tested", label: "Tested", value: {locale: "en-en"} },
+      { key: "ca-es", label: "Tested ca-es", value: {locale: "ca-es"} },
+      { key: "es-es", label: "Tested es-es", value: {locale: "es-es"} },
+    ],
+  };
+});
+
 jest.mock("./Widget", () => {
   return {
     __esModule: true,
@@ -22,12 +69,6 @@ test('uses default language `ca-es` when navigator language is unknown', () => {
   expect(getDefaultLanguage([])).toBe('ca-es');
 });
 
-test('renders copyright link', () => {
-  const { getByText } = render(<App onLoadNewServiceWorkerAccept={() => {}} />);
-  const linkElement = getByText(/Source code of/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
 test('detects navigator language', () => {
   const languageGetter = jest.spyOn(window.navigator, 'language', 'get');
   languageGetter.mockReturnValue('es-ES');
@@ -42,25 +83,102 @@ test('detects new service worker', async () => {
   await act( async () => {
     const event = new CustomEvent('onNewServiceWorker', { detail: { registration: true } });
     fireEvent(document, event);
-    const updateElement = await app.findByText('1'); // Detect menu badge
-    expect(updateElement).toBeInTheDocument();
+
+    // Execute events block in current event loop
+    await delay(0);
+
+    const detectedNewSW = app.getByTestId("dashboard-mock-sw-detected");
+    expect(detectedNewSW).toHaveTextContent("true");
   });
 });
 
-test('translates app into detected language', async () => {
-  const languageGetter = jest.spyOn(window.navigator, 'language', 'get')
-  await act( async () => {
-    languageGetter.mockReturnValue('en-US');
-    const app = render(<App onLoadNewServiceWorkerAccept={()=>{}} />);
-    const [codeElement,] = await app.findAllByText('Refactored');
-    expect(codeElement).toBeInTheDocument();
-    // Ensure app is fully re-created
-    app.unmount();
+test('renders mocked dashboard', () => {
+  const app = render(<App onLoadNewServiceWorkerAccept={() => {}} />);
+  const element = app.getByTestId("dashboard-mock");
+  expect(element).toBeInTheDocument();
+});
+
+test('detects new service worker acceptance', async () => {
+  const onLoadNewServiceWorkerAccept = jest.fn();
+  let app;
+  act(() => {
+    app = render(<App onLoadNewServiceWorkerAccept={ onLoadNewServiceWorkerAccept } />);
   });
+
   await act( async () => {
-    languageGetter.mockReturnValue('ca-ES');
-    const app = render(<App onLoadNewServiceWorkerAccept={()=>{}} />);
-    const [codeElement,] = await app.findAllByText('Refactoritzada');
-    expect(codeElement).toBeInTheDocument();
+    // Fire event for new service worker detection
+    const event = new CustomEvent('onNewServiceWorker', { detail: { registration: "tested" } });
+    fireEvent(document, event);
+
+    // User accepts it
+    const button = app.getByTestId("dashboard-mock-fn-accept-sw");
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    // Execute events block in current event loop
+    await delay(0);
+
+    expect(onLoadNewServiceWorkerAccept).toHaveBeenCalledTimes(1);
+    expect(onLoadNewServiceWorkerAccept).toHaveBeenCalledWith("tested");
+  });
+});
+
+test('detects user changed language', async () => {
+  let app;
+  act(() => {
+    app = render(<App onLoadNewServiceWorkerAccept={() => {}} />);
+  });
+
+  await act( async () => {
+    // User selects a language
+    const button = app.getByTestId("dashboard-mock-fn-language-change");
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    // Execute events block in current event loop
+    await delay(0);
+
+    const status = app.getByTestId("dashboard-mock-language");
+    expect(status).toHaveTextContent("tested");
+  });
+});
+
+test('detects user changed theme', async () => {
+  let app;
+  act(() => {
+    app = render(<App onLoadNewServiceWorkerAccept={() => {}} />);
+  });
+
+  await act( async () => {
+    // User selects a theme
+    const button = app.getByTestId("dashboard-mock-fn-theme-change");
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    // Execute events block in current event loop
+    await delay(0);
+
+    const status = app.getByTestId("dashboard-mock-theme");
+    expect(status).toHaveTextContent("tested");
+  });
+});
+
+test('detects user visited tutorial', async () => {
+  let app;
+  act(() => {
+    app = render(<App onLoadNewServiceWorkerAccept={() => {}} />);
+  });
+
+  await act( async () => {
+    // User selects a theme
+    const button = app.getByTestId("dashboard-mock-fn-tutorial-seen");
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    // Execute events block in current event loop
+    await delay(0);
+
+    const status = app.getByTestId("dashboard-mock-tutorial-seen");
+    expect(status).toHaveTextContent("tested");
   });
 });
---
+++ app/src/tests/App.int.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, createEvent, fireEvent, act, screen } from '@testing-library/react';
+
+import '../testSetup';
+import App from '../App';
+
+test('renders copyright link', () => {
+  const { getByText } = render(<App onLoadNewServiceWorkerAccept={() => {}} />);
+  const linkElement = getByText(/Source code of/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('menu shows detected new service worker', async () => {
+  let app;
+  act( () => {
+    app = render(<App onLoadNewServiceWorkerAccept={()=>{}} />);
+  });
+  await act( async () => {
+    const event = new CustomEvent('onNewServiceWorker', { detail: { registration: true } });
+    fireEvent(document, event);
+    const updateElement = await app.findByText('1'); // Detect menu badge
+    expect(updateElement).toBeInTheDocument();
+  });
+});
+
+test('translates app into detected language', async () => {
+  const languageGetter = jest.spyOn(window.navigator, 'language', 'get')
+  await act( async () => {
+    languageGetter.mockReturnValue('en-US');
+    const app = render(<App onLoadNewServiceWorkerAccept={()=>{}} />);
+    const [codeElement,] = await app.findAllByText('Refactored');
+    expect(codeElement).toBeInTheDocument();
+    // Ensure app is fully re-created to re-run the constructor
+    app.unmount();
+  });
+  await act( async () => {
+    languageGetter.mockReturnValue('ca-ES');
+    const app = render(<App onLoadNewServiceWorkerAccept={()=>{}} />);
+    const [codeElement,] = await app.findAllByText('Refactoritzada');
+    expect(codeElement).toBeInTheDocument();
+  });
+});
