73e73cea8b88b8cb65a530c7345b98066d49131b
emibcn <emi@algorismia.com>
2021-03-28 16:37:57 +0200

    Enhance ErrorCatcher: - Split logic from presentation - Use new presentation as default, but allow overriding it - Allow re-creating the component instead of reloading the entire page:   - Count the errors detected on the wrapped component   - Use different button text   - Clean error on retry, so children needs to be rendered again   - Add a hook `onRetry`, triggered after user clicks the retry button

--- app/src/ErrorCatcher.jsx
+++ app/src/ErrorCatcher.jsx
@@ -5,18 +5,68 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSyncAlt as faRefresh } from '@fortawesome/free-solid-svg-icons'
 import { translate } from 'react-translate'
 
+
+const ShowErrorDefault = (props) => {
+  const {onRetry, reloadOnRetry, errorMessage, errorStack, count, t} = props;
+  return (
+    <>
+      <h1>{ t("Something went wrong :(") }</h1>
+      <button
+        onClick={ onRetry }
+        style={{ padding: '1em' }}
+      >
+        <FontAwesomeIcon
+          icon={ faRefresh }
+          style={{ margin: '0 .8em 0 .2em' }}
+        />
+        { reloadOnRetry
+            ? t("Try reloading the app to recover from it")
+            : t("Try recreating this component to recover from the error")
+        }
+      </button>
+      <div style={{ textAlign: 'left' }}>
+        <h2>{ errorMessage }</h2>
+        { reloadOnRetry ? null : (
+            <p>{ t("Counter") }: { count }</p>
+        )}
+        <details style={{ whiteSpace: "pre-wrap" }}>
+          { errorStack }
+        </details>
+      </div>
+    </>
+  )
+}
+
+ShowErrorDefault.defaultProps = {
+  t: text => text,
+};
+
+ShowErrorDefault.propTypes = {
+  onRetry: PropTypes.func.isRequired,
+  reloadOnRetry: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string.isRequired,
+  errorStack: PropTypes.string.isRequired,
+  count: PropTypes.number.isRequired,
+  t: PropTypes.func.isRequired,
+};
+
 // Catches unhandled errors:
 // - Show it to user
 // - Send them to GA to help improve app
 class ErrorCatcher extends React.PureComponent {
 
+  initialState = {
+    hasError: false,
+    errorMessage: '',
+    errorStack: '',
+    info: '',
+  }
+
   constructor(props) {
     super(props);
     this.state = {
-      hasError: false,
-      errorMessage: '',
-      errorStack: '',
-      info: '',
+      ...this.initialState,
+      count: 0,
     };
   }
 
@@ -32,54 +82,65 @@ class ErrorCatcher extends React.PureComponent {
   // Catch unhandled errors and send them to help improving the app
   componentDidCatch(error, info) {
 
-    this.setState({
-      info: info.componentStack,
-    });
-
-    this.sendEvent('Unhandled Error');
+    this.setState(
+      ({count}) => ({
+        info: info.componentStack,
+        count: count + 1,
+      }),
+      () => this.sendEvent('Unhandled Error')
+    );
   }
 
   reloadApp = () => {
-    this.sendEvent('Reload from Error');
+    this.sendEvent('Reload page from Error');
 
     // Reload app
     window.location.reload();
   }
 
+  retry = () => {
+    if (this.props.reloadOnRetry) {
+      this.reloadApp();
+    }
+    else {
+      this.sendEvent('Reload component from Error');
+      this.props.onRetry(this.state.count);
+      this.setState({
+        ...this.initialState
+      });
+    }
+  }
+
   // Send event to GA
-  sendEvent(event) {
-    this.props.sendEvent(event, this.props.origin, this.state.errorMessage);
+  sendEvent = (event) => {
+    const {
+      props: {sendEvent, origin},
+      state: {errorMessage, count}
+    } = this;
+    sendEvent(
+      event,
+      origin,
+      `${errorMessage} (${count})`);
   }
 
   render() {
     if (this.state.hasError) {
-      const { t } = this.props;
-      const { errorMessage, errorStack } = this.state;
+      const { reloadOnRetry, ShowError } = this.props;
+      const { errorMessage, errorStack, count } = this.state;
 
       // Show error to user
       return (
-        <>
-          <h1>{ t('Something went wrong :(') }</h1>
-          <button
-            onClick={ this.reloadApp }
-            style={{ padding: '1em' }}
-          >
-            <FontAwesomeIcon
-              icon={ faRefresh }
-              style={{ margin: '0 .8em 0 .2em' }}
-            />
-            { t("Try reloading the app to recover from it") }
-          </button>
-          <div style={{ textAlign: 'left' }}>
-            <h3>{ errorMessage }</h3>
-            <details style={{ whiteSpace: "pre-wrap" }}>
-              { errorStack }
-            </details>
-          </div>
-        </>
-      );
+        <ShowError
+          onRetry={ this.retry }
+          reloadOnRetry={ reloadOnRetry }
+          errorMessage={ errorMessage }
+          errorStack={ errorStack }
+          count={ count }
+        />
+      )
     }
 
+    // Execute contained components
     return this.props.children
   }
 }
@@ -87,14 +148,21 @@ class ErrorCatcher extends React.PureComponent {
 ErrorCatcher.defaultProps = {
   origin: 'No name',
   sendEvent: () => {},
+  onRetry: () => {},
+  reloadOnRetry: true,
+  ShowError: translate('ErrorCatcher')(ShowErrorDefault),
 };
 
 ErrorCatcher.propTypes = {
   origin: PropTypes.string.isRequired,
   sendEvent: PropTypes.func.isRequired,
+  onRetry: PropTypes.func.isRequired,
+  reloadOnRetry: PropTypes.bool.isRequired,
+  ShowError: PropTypes.elementType.isRequired,
 };
 
-const withErrorCatcher = (origin, component) => <ErrorCatcher {...{ origin , key: origin }}>{ component }</ErrorCatcher>;
+const withErrorCatcher = (origin, component) =>
+  <ErrorCatcher {...{ origin , key: origin }}>{ component }</ErrorCatcher>;
 
-export default translate('ErrorCatcher')(ErrorCatcher);
-export {withErrorCatcher};
+export default ErrorCatcher;
+export { withErrorCatcher, ShowErrorDefault };
