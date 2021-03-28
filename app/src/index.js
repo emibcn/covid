5fed195f6d3d75580b1baaa6f05083af546b04c4
emibcn <emi@algorismia.com>
2021-03-28 16:47:06 +0200

    WebVitals: Log to console with a bit of style

--- app/src/index.js
+++ app/src/index.js
@@ -30,4 +30,7 @@ serviceWorkerRegistration.register({
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+reportWebVitals(
+  // Log to console with a bit of style
+  ({name, ...props}) => console.log(`%c${name}`, "font-weight: bold", props)
+);
