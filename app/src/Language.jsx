6b16983209ae029296114b25715bbd772c226b47
emibcn <emi@algorismia.com>
2021-04-24 21:45:06 +0200

    Transform `<Language>` into a functional component

--- app/src/Language.jsx
+++ app/src/Language.jsx
@@ -9,46 +9,45 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLanguage } from "@fortawesome/free-solid-svg-icons";
 import available from "./i18n/available";
 
-class Language extends React.Component {
-  render() {
-    const { t, language, onLanguageChange } = this.props;
+const Language = (props) => {
+  const { t, language, onLanguageChange } = props;
+  const handleLanguageChange = (e) => onLanguageChange(e.target.value);
 
-    const filteredLanguages = available.reduce((languages, current) => {
-      if (!languages.find((item) => item.label === current.label)) {
-        return languages.concat([current]);
-      } else {
-        return languages;
-      }
-    }, []);
+  const filteredLanguages = available.reduce((languages, current) => {
+    if (!languages.find((item) => item.label === current.label)) {
+      return languages.concat([current]);
+    } else {
+      return languages;
+    }
+  }, []);
 
-    return (
+  return (
+    <>
+      <h1 id="modal_heading">
+        <FontAwesomeIcon icon={faLanguage} style={{ marginRight: ".5em" }} />
+        {t("Language")}
+      </h1>
       <>
-        <h1 id="modal_heading">
-          <FontAwesomeIcon icon={faLanguage} style={{ marginRight: ".5em" }} />
-          {t("Language")}
-        </h1>
-        <>
-          <FormControl component="fieldset">
-            <RadioGroup
-              aria-label="language"
-              name="language"
-              value={language}
-              onChange={(e) => onLanguageChange(e.target.value)}
-            >
-              {filteredLanguages.map((ele, index) => (
-                <FormControlLabel
-                  key={index}
-                  value={ele.key}
-                  control={<Radio />}
-                  label={ele.label}
-                />
-              ))}
-            </RadioGroup>
-          </FormControl>
-        </>
+        <FormControl component="fieldset">
+          <RadioGroup
+            aria-label="language"
+            name="language"
+            value={language}
+            onChange={ handleLanguageChange }
+          >
+            {filteredLanguages.map((ele, index) => (
+              <FormControlLabel
+                key={index}
+                value={ele.key}
+                control={<Radio />}
+                label={ele.label}
+              />
+            ))}
+          </RadioGroup>
+        </FormControl>
       </>
-    );
-  }
+    </>
+  );
 }
 
 export default translate("Language")(Language);
