d8781b2b82d85f3bc4884316f11ce398bc65f74f
emibcn <emi@algorismia.com>
2021-10-17 12:51:31 +0200

    #237 - Fix asyncComponent

--- app/src/asyncComponent.jsx
+++ app/src/asyncComponent.jsx
@@ -5,8 +5,8 @@ import Loading from './Loading'
 // From https://gist.github.com/aadii104/0ed32d46b70ed2f4037dbbbbc1477edc
 const asyncComponent =
   (importComponent, module = 'default') =>
-    (props) => {
-    // Will save the component into a state
+    function AsyncComponent (props) {
+      // Will save the component into a state
       const [C, setComponent] = React.useState(false)
 
       // Call module import (async), wait for
@@ -30,33 +30,35 @@ const asyncComponent =
       return C ? <C {...props} /> : <Loading />
     }
 
-const asyncModuleComponent = (importModule, modules, Wrapped) => (props) => {
-  // Will save components into a state
-  const [components, setComponents] = React.useState(false)
+const asyncModuleComponent =
+  (importModule, modules, Wrapped) =>
+    function AsyncModuleComponent (props) {
+      // Will save components into a state
+      const [components, setComponents] = React.useState(false)
 
-  // Call module import (async), wait for
-  // results and set components state
-  //
-  // useEffect' Callback must not be async, so create an internal
-  // anonymous and async function and call it immediatelly
-  React.useEffect(() => {
-    (async () => {
-      // Import all components
-      const comps = await importModule()
-      // Create a new object only with the modules
-      // defined in HOC argument `modules`
-      const filtered = Object.fromEntries(
-        Object.entries(comps).filter(([module]) => modules.includes(module))
-      )
-      setComponents(filtered)
-      // No return, no unsubscribe
-    })()
-    // Only call it once
-  }, [])
+      // Call module import (async), wait for
+      // results and set components state
+      //
+      // useEffect' Callback must not be async, so create an internal
+      // anonymous and async function and call it immediatelly
+      React.useEffect(() => {
+        (async () => {
+          // Import all components
+          const comps = await importModule()
+          // Create a new object only with the modules
+          // defined in HOC argument `modules`
+          const filtered = Object.fromEntries(
+            Object.entries(comps).filter(([module]) => modules.includes(module))
+          )
+          setComponents(filtered)
+          // No return, no unsubscribe
+        })()
+        // Only call it once
+      }, [])
 
-  // Once loaded, pass components to wrapped as a prop
-  return !components ? <Loading /> : <Wrapped {...props} {...{ components }} />
-}
+      // Once loaded, pass components to wrapped as a prop
+      return !components ? <Loading /> : <Wrapped {...props} {...{ components }} />
+    }
 
 export default asyncComponent
 export { asyncModuleComponent }
