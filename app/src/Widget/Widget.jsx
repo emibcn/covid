01cd5b289cd96f5d242f69c57a89d8b3dba67fa3
emibcn <emi@algorismia.com>
2021-10-20 20:15:20 +0200

    Remove unneeded argument

--- app/src/Widget/Widget.jsx
+++ app/src/Widget/Widget.jsx
@@ -17,7 +17,7 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import WidgetActions from './Actions'
 import ErrorCatcher from '../ErrorCatcher'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
     height: '100%'
   },
@@ -29,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
       cursor: 'move'
     }
   }
-}))
+})
 
 // HOC to create widgets easily
 const withWidget = (sectionsOrig) => {
